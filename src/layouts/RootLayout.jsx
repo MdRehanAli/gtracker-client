0391c6a9561720f4b813d23eee4c44a7dfea0476
MdRehanAli <rehanali21d@gmail.com>
2025-12-08 19:49:44 +0600

    Added basic Navbar setup

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Outlet } from 'react-router';
+import Navbar from '../pages/Shared/Navbar/Navbar';
 
 const RootLayout = () => {
     return (
         <div>
-            <Outlet></Outlet>
+            <Navbar></Navbar>
+            <div className='max-w-7xl mx-auto w-11/12'>
+                <Outlet></Outlet>
+            </div>
         </div>
     );
 };
