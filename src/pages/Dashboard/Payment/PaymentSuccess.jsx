b7e0aa26d72a4fe3b64f4874e11fbd161e07de61
MdRehanAli <rehanali21d@gmail.com>
2025-12-15 21:20:31 +0600

    Added Succesfull Payment Ids

--- src/pages/Dashboard/Payment/PaymentSuccess.jsx
+++ src/pages/Dashboard/Payment/PaymentSuccess.jsx
@@ -1,10 +1,11 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSearchParams } from 'react-router';
 import useAxiosSecure from '../../../hooks/useAxiosSecure';
 
 const PaymentSuccess = () => {
 
     const [searchParams] = useSearchParams();
+    const [paymentInfo, setPaymentInfo] = useState({})
     const sessionId = searchParams.get('session_id');
     const axiosSecure = useAxiosSecure();
 
@@ -15,6 +16,10 @@ const PaymentSuccess = () => {
             axiosSecure.patch(`/payment-success?session_id=${sessionId}`)
                 .then(res => {
                     console.log(res.data);
+                    setPaymentInfo({
+                        trackingId: res.data.trackingId,
+                        transactionId: res.data.trackingId
+                    })
                 })
         }
     }, [axiosSecure, sessionId])
@@ -22,6 +27,8 @@ const PaymentSuccess = () => {
     return (
         <div>
             <h1 className='text-4xl'>Payment Successful</h1>
+            <p>Your Transaction Id is: {paymentInfo.transactionId}</p>
+            <p>Your Tracking Id is: {paymentInfo.trackingId}</p>
         </div>
     );
 };
