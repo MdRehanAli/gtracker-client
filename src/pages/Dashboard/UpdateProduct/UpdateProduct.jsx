2e0ec5bf26e05876107f24d92822f01bd2b75a4f
MdRehanAli <rehanali21d@gmail.com>
2025-12-22 23:14:34 +0600

    Added Update Product

--- src/pages/Dashboard/UpdateProduct/UpdateProduct.jsx
+++ src/pages/Dashboard/UpdateProduct/UpdateProduct.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { useForm } from 'react-hook-form';
 import { HiMiniPencilSquare } from 'react-icons/hi2';
 import { useLoaderData } from 'react-router';
+import Swal from 'sweetalert2';
+import useAxiosSecure from '../../../hooks/useAxiosSecure';
 
 const UpdateProduct = () => {
 
@@ -11,49 +13,44 @@ const UpdateProduct = () => {
 
     const { register, handleSubmit, formState: { errors } } = useForm();
 
+    const axiosSecure = useAxiosSecure();
 
-    const handleUpdateProduct = id => {
+
+    const handleUpdateProduct = data => {
         const productUpdateInfo = {
-            name: products.name,
-            image: products.image,
-            largeDescription: products.largeDescription,
-            category: products.category,
-            price: products.price,
-            availableQuantity: products.availableQuantity,
-            minimumOrder: products.minimumOrder,
-            video: products.video,
-            paymentOptions: products.paymentOptions
+            name: data.name,
+            largeDescription: data.largeDescription,
+            category: data.category,
+            price: Number(data.price),
+            availableQuantity: Number(data.availableQuantity),
+            minimumOrder: Number(data.minimumOrder),
+            video: data.video,
+            paymentOptions: data.paymentOptions
         }
 
         Swal.fire({
             title: "Are you sure?",
-            text: "You won't be able to revert this!",
+            text: "You want to update this product?",
             icon: "warning",
             showCancelButton: true,
-            confirmButtonColor: "#3085d6",
-            cancelButtonColor: "#d33",
-            confirmButtonText: "Yes, delete it!"
+            confirmButtonText: "Yes, update it!"
         }).then((result) => {
             if (result.isConfirmed) {
 
-                axiosSecure.patch(`all-products/${id}`, productUpdateInfo)
+                axiosSecure
+                    .patch(`/all-products/${_id}`, productUpdateInfo)
                     .then(res => {
-                        console.log(res.data);
-
-                        if (res.data.modifiedCount) {
-                            refetch();
-
-                            Swal.fire({
-                                title: "Deleted!",
-                                text: "Your Product has been deleted.",
-                                icon: "success"
-                            });
-
+                        if (res.data.modifiedCount > 0) {
+                            Swal.fire("Updated!", "Product updated successfully", "success");
                         }
                     })
+                    .catch(err => {
+                        console.error(err);
+                        Swal.fire("Error!", "Update failed", "error");
+                    });
             }
         });
-    }
+    };
 
 
     return (
@@ -67,17 +64,17 @@ const UpdateProduct = () => {
                         <div className='flex-1'>
                             {/* Product Title Field  */}
                             <label className="text-black label mt-2">Product Title</label>
-                            <input type="text" {...register('name', { required: true })} className="input w-full" placeholder="Product Name" />
+                            <input type="text" {...register('name', { required: true })} defaultValue={name} className="input w-full" placeholder="Product Name" />
                             {errors.name?.type === "required" && <p className='text-red-500'>Product Title is Required</p>}
 
                             {/* Product Description Field  */}
                             <label className="text-black label mt-2">Product Description</label>
-                            <textarea rows={4} placeholder="Product Description" className='p-3 w-full border border-gray-300 rounded-sm text-sm bg-base-100' name="" {...register('largeDescription', { required: true })} id=""></textarea>
+                            <textarea rows={4} placeholder="Product Description" className='p-3 w-full border border-gray-300 rounded-sm text-sm bg-base-100' name="" {...register('largeDescription', { required: true })} defaultValue={largeDescription} id=""></textarea>
                             {errors.largeDescription?.type === "required" && <p className='text-red-500'>Product Description is Required</p>}
 
                             {/* Category Field  */}
                             <label className="text-black label">Select Category</label>
-                            <select {...register('category', { required: true })} className="select w-full">
+                            <select {...register('category', { required: true })} defaultValue={category} className="select w-full">
                                 <option >Choose a Category</option>
                                 <option value="shirt">Shirt</option>
                                 <option value="pant">Pant</option>
@@ -88,7 +85,7 @@ const UpdateProduct = () => {
 
                             {/* Price Field  */}
                             <label className="text-black label mt-2">Price</label>
-                            <input type="number" {...register('price', { required: true })} className="input w-full" placeholder="Price" />
+                            <input type="number" {...register('price', { required: true })} defaultValue={price} className="input w-full" placeholder="Price" />
                             {errors.price?.type === "required" && <p className='text-red-500'>Price is Required</p>}
 
                         </div>
@@ -96,26 +93,26 @@ const UpdateProduct = () => {
                         <div className='flex-1'>
                             {/* Available Quantity Field  */}
                             <label className="text-black label mt-2">Available Quantity</label>
-                            <input type="number" {...register('availableQuantity', { required: true })} className="input w-full" placeholder="Available Quantity" />
+                            <input type="number" {...register('availableQuantity', { required: true })} defaultValue={availableQuantity} className="input w-full" placeholder="Available Quantity" />
                             {errors.availableQuantity?.type === "required" && <p className='text-red-500'>Available Quantity is Required</p>}
 
                             {/* Minimum Order Quantity (MOQ) Field  */}
                             <label className="text-black label mt-2">Minimum Order Quantity (MOQ)</label>
-                            <input type="number" {...register('minimumOrder', { required: true })} className="input w-full" placeholder="Minimum Order Quantity (MOQ)" />
+                            <input type="number" {...register('minimumOrder', { required: true })} defaultValue={minimumOrder} className="input w-full" placeholder="Minimum Order Quantity (MOQ)" />
                             {errors.minimumOrder?.type === "required" && <p className='text-red-500'>Minimum Order Quantity (MOQ) is Required</p>}
 
                             {/* Product Image Field  */}
-                            <label className="text-black label mt-2">Product Image</label>
+                            {/* <label className="text-black label mt-2">Product Image</label>
                             <input type="file" {...register('image', { required: true })} className="file-input w-full" placeholder="Your Photo" />
-                            {errors.image?.type === "required" && <p className='text-red-500'>Product Image is Required.</p>}
+                            {errors.image?.type === "required" && <p className='text-red-500'>Product Image is Required.</p>} */}
 
                             {/* Demo Video Link Field  */}
                             <label className="text-black label mt-2">Demo Video Link</label>
-                            <input type="text" {...register('video')} className="input w-full" placeholder="Demo Video Link" />
+                            <input type="text" {...register('video')} defaultValue={video} className="input w-full" placeholder="Demo Video Link" />
 
                             {/* Payment Options Field  */}
                             <label className="text-black label mt-2">Payment Options</label>
-                            <select {...register('paymentOptions', { required: true })} className="select w-full">
+                            <select {...register('paymentOptions', { required: true })} defaultValue={paymentOptions} className="select w-full">
                                 <option disabled={true}>Choose a Payment Options</option>
                                 <option value="cashOnDelivery">Cash on Delivery </option>
                                 <option value="payFast">PayFast</option>
