825c31cf76512e7e2a36b5552d138234e0bc2ddc
MdRehanAli <rehanali21d@gmail.com>
2025-12-24 02:19:53 +0600

    Added Search functionality

--- src/pages/Dashboard/ManageProducts/ManageProducts.jsx
+++ src/pages/Dashboard/ManageProducts/ManageProducts.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { HiMiniPencilSquare } from 'react-icons/hi2';
 import useAuth from '../../../hooks/useAuth';
 import useAxiosSecure from '../../../hooks/useAxiosSecure';
@@ -8,8 +8,19 @@ import { Link } from 'react-router';
 
 const ManageProducts = () => {
 
+
     const { user } = useAuth();
     const axiosSecure = useAxiosSecure();
+
+    const [searchText, setSearchText] = useState('');
+
+    const { data: allProducts = [] } = useQuery({
+        queryKey: ['all-products', searchText],
+        queryFn: async () => {
+            const res = await axiosSecure.get(`/all-products?searchText=${searchText}`);
+            return res.data;
+        }
+    })
 
     const { data: products = [], refetch } = useQuery({
         queryKey: ['manageProducts', user?.email],
@@ -58,6 +69,24 @@ const ManageProducts = () => {
                 <div className='flex justify-center text-7xl'><HiMiniPencilSquare className='p-4 bg-gray-300 rounded-3xl' /></div>
                 <h1 className='text-3xl md:text-5xl font-bold text-center my-5'>Manage Products</h1>
 
+                <div className='flex justify-center'>
+                    <label className="input w-full md:w-1/2 h-14 mb-8">
+                        <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                            <g
+                                strokeLinejoin="round"
+                                strokeLinecap="round"
+                                strokeWidth="2.5"
+                                fill="none"
+                                stroke="currentColor"
+                            >
+                                <circle cx="11" cy="11" r="8"></circle>
+                                <path d="m21 21-4.3-4.3"></path>
+                            </g>
+                        </svg>
+                        <input onChange={(e) => setSearchText(e.target.value)} type="search" className="grow" placeholder="Search Product Name/Category" />
+                    </label>
+                </div>
+
                 <div className="overflow-x-auto">
                     <table className="table table-zebra text-center">
                         {/* head */}
@@ -74,7 +103,7 @@ const ManageProducts = () => {
                         <tbody>
 
                             {
-                                products.map((product, index) => <tr key={product._id}>
+                                allProducts.map((product, index) => <tr key={product._id}>
                                     <th>{index + 1}</th>
                                     <td>
                                         <div className="flex justify-center items-center gap-3">
