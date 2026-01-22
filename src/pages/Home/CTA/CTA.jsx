f91d082681289267d5832d1adf04df164c687281
MdRehanAli <rehanali21d@gmail.com>
2026-01-22 14:49:33 +0600

    update join

--- src/pages/Home/CTA/CTA.jsx
+++ src/pages/Home/CTA/CTA.jsx
@@ -3,16 +3,18 @@ import { Link } from 'react-router';
 
 const CTA = () => {
     return (
-        <section className="py-24 bg-accent relative overflow-hidden">
-            <div className="absolute inset-0 opacity-5 bg-[radial-gradient(circle_at_2px_2px,white_1px,transparent_0)] [background-size:40px_40px]"></div>
-            <div className="w-11/12 max-w-7xl mx-auto text-center relative z-10">
-                <h2 className="text-3xl md:text-4xl font-black text-primary mb-4 tracking-tight">Revolutionize Your Supply Chain Today</h2>
-                <p className="text-xl text-secondary mb-8 font-medium max-w-3xl mx-auto">
-                    Join 250+ enterprise labels scaling faster, cleaner, and smarter with GTracker.
-                </p>
-                <div className="flex flex-wrap justify-center gap-6">
-                    <Link to="/all-products" className="btn btn-primary">Order Now</Link>
-                    <Link to="/contact" className="btn btn-primary">Connect with GTracker</Link>
+        <section className="bg-accent relative overflow-hidden bg-gradient-to-r from-primary/30 to-accent">
+            <div className='lg:p-20 md:p-10 p-5 py-10'>
+                <div className="absolute inset-0 opacity-10 bg-[radial-gradient(circle_at_2px_2px,white_1px,transparent_0)] [background-size:40px_40px]"></div>
+                <div className="w-11/12 max-w-7xl mx-auto text-center relative z-10">
+                    <h2 className="text-3xl md:text-4xl font-black text-primary mb-4 tracking-tight">Revolutionize Your Supply Chain Today</h2>
+                    <p className="text-xl text-secondary mb-8 font-medium max-w-3xl mx-auto">
+                        Join 250+ enterprise labels scaling faster, cleaner, and smarter with GTracker.
+                    </p>
+                    <div className="flex flex-wrap justify-center gap-6">
+                        <Link to="/all-products" className="btn btn-primary">Order Now</Link>
+                        <Link to="/contact" className="btn btn-primary">Connect with GTracker</Link>
+                    </div>
                 </div>
             </div>
         </section>
--- src/pages/Home/WinterShowcase/WinterShowcase.jsx
+++ src/pages/Home/WinterShowcase/WinterShowcase.jsx
@@ -19,28 +19,28 @@ const WinterShowcase = () => {
                     </p>
                 </div>
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
-                    <div className="aspect-[3/4] rounded-xl overflow-hidden">
+                    <div className="aspect-[3/4] animate-[pulse_2s_linear_infinite] hover:animate-none rounded-xl overflow-hidden">
                         <img
                             alt="Lookbook"
                             className="w-full h-full object-cover"
                             src="https://lh3.googleusercontent.com/aida-public/AB6AXuAA5_afg8TcKhBUTT5PdRbe5qnYgL7QYagRhAJ_1Avoc0foeZSaWBzRi6hCzX8Z8WJilcGa6KtJ6GvobdsqqahXR8Oe9H1msygC9wWjbmqo6EOmARRuUpZLl2O75NEYZ7oAxVHw5Fy9UAjmzWiJAAwAiCM2gZrElwGjJKW0nP311JqSoII-4AShuziL_tSkFHNhq5PcGi6_qyL-u5e8wxB-lcFZ4FSKqg3hhJZS9AkVG54oLNOCRd48z-x6vVb02_7p0xh464e1SE_h"
                         />
                     </div>
-                    <div className="aspect-[3/4] rounded-xl overflow-hidden lg:mt-12">
+                    <div className="aspect-[3/4] animate-[pulse_3s_linear_infinite] hover:animate-none rounded-xl overflow-hidden lg:mt-12">
                         <img
                             alt="Lookbook"
                             className="w-full h-full object-cover"
                             src="https://lh3.googleusercontent.com/aida-public/AB6AXuCOPYf0hsbUv2tZcXvw0kBjYzH-M7Ezwojp0wqVDpoSAS-RbRj4TNnDYVuM4_BL39agOILC7olxE5Um2Po36BHUgsIGeUHM-giNjGzYadAty7ciuM6wGUYw2l4f0wZ7Ddfnq9k7JVIg8vV_8SLRcAo6--MQPNkngc_knhiKYTT5yu9Jb4cTzeH1wAcLUGztbiK_zN6GhO1IxztpRXNSEvPfAmxpziggUBVuhB39dJC7eWTjxkoRYX5uTCGLtptSA86gfCDJJU4q-hVQ"
                         />
                     </div>
-                    <div className="aspect-[3/4] rounded-xl overflow-hidden">
+                    <div className="aspect-[3/4] animate-[pulse_2s_linear_infinite] hover:animate-none rounded-xl overflow-hidden">
                         <img
                             alt="Lookbook"
                             className="w-full h-full object-cover"
                             src="https://lh3.googleusercontent.com/aida-public/AB6AXuBvtMH6_Cd_wUcByfsXutwSkIgCZU9ylvOK0eXo_6GmfIgvY2Ax26r8hIZ6UFNv2nf2dGrw5dAOECuKlOdv_zXE_TQvQ9_ZFI3yCMbbrRAgO64Inm0PHcGZJEzNSJoweHYccSpMlrymsFdE_0IaIE7RHOgsILM0xixRd92spfmJQ_2a11EeXyBt47Y6uMtyS-TTTE_TPdA9DmobXtFunDMXEs5uy_Eiz0jETEssxa0M6hpzQvYpJ1eGhzorSB2rAx0wHbe62Vn0YumZ"
                         />
                     </div>
-                    <div className="aspect-[3/4] rounded-xl overflow-hidden lg:mt-12">
+                    <div className="aspect-[3/4] animate-[pulse_3s_linear_infinite] hover:animate-none rounded-xl overflow-hidden lg:mt-12">
                         <img
                             alt="Lookbook"
                             className="w-full h-full object-cover"
