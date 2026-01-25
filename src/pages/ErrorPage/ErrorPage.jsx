21c19a402637fb72dfe32a1f57f0f55ab7b7fd6e
MdRehanAli <rehanali21d@gmail.com>
2026-01-26 03:16:00 +0600

    Update Errorpage

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -7,13 +7,13 @@ const ErrorPage = () => {
         <div className='flex flex-col md:flex-row justify-center items-center gap-8 md:gap-20 md:p-20'>
             <img src={ErrorImage} alt="Error Page" />
             <div className='text-center md:text-left'>
-                <h1 className='text-[#ffcc9e] text-7xl md:text-9xl font-bold mb-5'>404</h1>
-                <h4 className='text-[#14344f] text-2xl md:text-4xl font-bold'>Page not found</h4>
-                <div className='my-5'>
+                <h1 className='text-primary text-7xl md:text-9xl font-bold mb-5'>404</h1>
+                <h4 className='text-primary/50 text-2xl md:text-4xl font-bold'>Page not found</h4>
+                <div className='my-5 text-secondary'>
                     <p>We couldn't find the page you were looking for.</p>
                     <p>It may have been moved or just doesn't exist.</p>
                 </div>
-                <Link to='/' className='btn'>Go Back home</Link>
+                <Link to='/' className='btn btn-primary'>Go Back home</Link>
             </div>
         </div>
     );
