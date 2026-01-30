0f5cfaf8e97f6f747865385b25f76eb0ab913ea9
MdRehanAli <rehanali21d@gmail.com>
2026-01-30 18:09:02 +0600

    Update loading

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -4,7 +4,7 @@ import { ScaleLoader } from 'react-spinners';
 const Loading = () => {
     return (
         <div className='min-h-screen flex justify-center items-center'>
-            <ScaleLoader size={60} color={'#dadada'} />
+            <ScaleLoader size={60} color={'#7C3AED'} />
         </div>
     );
 };
