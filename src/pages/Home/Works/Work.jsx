35683dfc890057f61ead254521b85ec40d169284
MdRehanAli <rehanali21d@gmail.com>
2026-03-12 03:32:55 +0600

    Update Works

--- src/pages/Home/Works/Work.jsx
+++ src/pages/Home/Works/Work.jsx
@@ -1,27 +1,30 @@
 import React from 'react';
 import { FaArrowRightLong } from 'react-icons/fa6';
+import FlyInMotion from '../../../components/Motion/FlyInMotion';
 
 const Work = ({ work }) => {
 
     const { name, description, image } = work;
 
     return (
-        <div className="p-5 md:p-10 rounded-xl bg-accent border border-primary/50 hover:border-primary/50 dark:hover:border-primary/50 hover:shadow-lg hover:shadow-primary/10 transform duration-200 hover:scale-105 flex flex-col gap-2">
-            <figure className="flex justify-center">
-                <img
-                    src={image}
-                    alt={name}
-                    className="w-20" />
-            </figure>
-            <div className="">
-                <h2 className="text-xl font-extrabold my-2 text-primary text-center">{name}</h2>
-                <p className='text-justify text-secondary leading-relaxed text-sm'><span className='font-bold'>Process:</span> {description}</p>
-                <div className="mt-2 flex items-center gap-2 justify-end text-primary">
-                    <button className="">Read More </button>
-                    <span><FaArrowRightLong /></span>
+        <FlyInMotion index={1}>
+            <div className="p-5 md:p-10 rounded-xl bg-accent border border-primary/50 hover:border-primary/50 dark:hover:border-primary/50 hover:shadow-lg hover:shadow-primary/10 transform duration-200 hover:scale-105 flex flex-col gap-2">
+                <figure className="flex justify-center">
+                    <img
+                        src={image}
+                        alt={name}
+                        className="w-20" />
+                </figure>
+                <div className="">
+                    <h2 className="text-xl font-extrabold my-2 text-primary text-center">{name}</h2>
+                    <p className='text-justify text-secondary leading-relaxed text-sm'><span className='font-bold'>Process:</span> {description}</p>
+                    <div className="mt-2 flex items-center gap-2 justify-end text-primary">
+                        <button className="">Read More </button>
+                        <span><FaArrowRightLong /></span>
+                    </div>
                 </div>
             </div>
-        </div>
+        </FlyInMotion>
     );
 };
 
--- src/pages/Home/Works/Works.jsx
+++ src/pages/Home/Works/Works.jsx
@@ -1,6 +1,7 @@
 import React, { use } from 'react';
 import Work from './Work';
 import { IoSettingsOutline } from "react-icons/io5";
+import FlyInMotion from '../../../components/Motion/FlyInMotion';
 
 const Works = ({ worksPromise }) => {
 
@@ -9,11 +10,18 @@ const Works = ({ worksPromise }) => {
     return (
         <div id='how' className=' my-20 mx-auto w-11/12 max-w-7xl'>
             <div className='text-center mb-12'>
-                <div className='flex justify-center items-center gap-2 mb-5'>
-                    <div className='flex justify-center text-3xl md:text-4xl text-primary animate-[spin_3s_linear_infinite] hover:animate-none'><IoSettingsOutline className='' /></div>
-                    <h1 className='text-3xl md:text-4xl font-bold text-primary'>How It Works</h1>
-                </div>
-                <p className='text-secondary mt-3 text-lg'>Do you know? How GTracker is Delivering The Highest Quality Fabrics?</p>
+                <FlyInMotion index={1}>
+                    <div className='flex justify-center items-center gap-2 mb-5'>
+                        <div className='flex justify-center text-3xl md:text-4xl text-primary animate-[spin_3s_linear_infinite] hover:animate-none'><IoSettingsOutline className='' /></div>
+                        <div>
+                            <h1 className='text-3xl md:text-4xl font-bold text-primary text-nowrap'>How It Works</h1>
+                            <div className='h-px w-36 md:w-42 bg-primary flex mx-auto animate-[ping_3s_linear_infinite] hover:animate-none'></div>
+                        </div>
+                    </div>
+                </FlyInMotion>
+                <FlyInMotion index={3}>
+                    <p className='text-secondary mt-3 text-lg'>Do you know? How GTracker is Delivering The Highest Quality Fabrics?</p>
+                </FlyInMotion>
             </div>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
                 {
